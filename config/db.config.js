2e81fa3355738659588ff697c8fb8cd0a688c0a6
Ankur Charan <ankurcharan98@gmail.com>
2020-09-21 04:06:23 +0530

    react init

--- config/db.config.js
+++ config/db.config.js
@@ -14,6 +14,21 @@ dbConn.connect((err) => {
 		throw err;
 	} 
 	console.log("Database Connected!");
+
+	dbConn.query(`CREATE table IF NOT EXISTS issues (
+			title varchar(300) NOT NULL,
+			body varchar(3000),
+			isopen int(1) DEFAULT(1),
+			id int auto_increment primary key
+		);`,
+		(err, results) => {
+
+			if(err) {
+
+				console.log(err);
+				console.log('Try setting up database manually');
+			}
+		})
 });
 
 module.exports = dbConn;
